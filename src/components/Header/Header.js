b1cf0ce4e5608215e7e4186b2eff7a4d5201211c
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-07-10 01:52:09 -0300

    hover logo in header with god is big

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Header.css';
-// import logo from '../logo.svg';
+import logo from '../../logo.svg';
 
 function Header() {
   return (
     <header className="App-header">
       <h1>Deus é Maior</h1>
-      { /* <img src={logo} className="App-logo" alt="logo Google" width="272" /> */ }
+      <img src={logo} className="App-logo" alt="logo Google" width="272" />
     </header>
   );
 }
