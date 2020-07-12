cda35a92d4c9798623b3aa0a6839ea3eb3fcbbf4
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-07-12 02:16:38 -0300

    component route do react com página de config

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import Menu from './components/Menu/Menu';
 import Header from './components/Header/Header';
 import Search from './components/Search/Search';
@@ -7,12 +12,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Menu />
-      <Header />
-      <Search />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Menu />
+        <Switch>
+          <Route path="/config">
+            <h1>Configurações</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Search />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
