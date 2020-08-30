6dfb13add5152176dc1376937297b7438b7cbe1a
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-08-30 00:40:32 -0300

    Component profile with popover content

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Profile.css';
 import foto from '../../sinatra.jpg';
 
 function Profile() {
+  const [show, setShow] = useState(false)
+  const profile = () => setShow(!show)
+
   return (
     <section className="App-profile">
-      <h1>2L</h1>
-      <img src={foto} className="App-profile-foto" alt="Foto" width="40" />
+      <h1 onClick={profile}>
+        2L
+        <img src={foto} className="App-profile-foto" alt="Foto" width="40" />
+      </h1>
+      <div className={ `App-profile__content${show ? ' show' : '' }`}>
+        <a href="http://lucas-x.web.app" target="_blank">
+          <img src={foto} className="App-profile-foto big" alt="Foto" />
+        </a>
+        <h1>Lucas Lima</h1>
+        <h2>lucasferreiralimax@gmail.com</h2>
+        <button type="button">Gerenciar sua Conta do Google</button>
+        <div className="divider">
+          <a href="https://policies.google.com/privacy" target="_blank">Política de Privacidade</a>
+          <div className="bullet">•</div>
+          <a href="https://policies.google.com/terms" target="_blank">Termos de Serviço</a>
+        </div>
+      </div>
     </section>
   );
 }
