cf7aa7297d8f8fc9e8c782b9109907a28f7904a1
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-09 03:08:14 -0300

    adjustment keyboard with functions button

--- src/components/Keyboard/Keyboard.js
+++ src/components/Keyboard/Keyboard.js
@@ -5,6 +5,9 @@ import './Keyboard.css';
 function Keyboard({ store, dispatch }) {
   const [translateY, setTranslateY] = useState(0)
   const [translateX, setTranslateX] = useState(0)
+  const [capslock, setCapslock] = useState(false)
+  const [shift, setShift] = useState(false)
+  const [ctrlalt, setCtrlalt] = useState(false)
 
   let holding = null;
   let onDraggingFunctionRef = null;
@@ -35,11 +38,269 @@ function Keyboard({ store, dispatch }) {
     }
   }
 
+  function onKeyVirtual(event) {
+    event.persist()
+    if(event.target.classList.contains('key')) {
+      let input = document.querySelector('.App-search-input')
+
+      switch(event.target.textContent) {
+        case 'backspace':
+          backspace(input)
+          break;
+        case 'whitespace':
+          insertAtCaret(input, ' ')
+          break;
+        case 'capslock':
+          setCapslock(!capslock)
+          capslockEvent(!capslock)
+          break;
+        case 'Ctrl+Alt':
+          setCtrlalt(!ctrlalt)
+          ctrlAltEvent(!ctrlalt)
+          console.log('Ctrl+Alt')
+          break;
+        case 'shift 1':
+        case 'shift 2':
+          setShift(!shift)
+          shiftEvent(!shift)
+          console.log('shitf 1')
+          break;
+        default:
+          insertAtCaret(input, event.target.textContent)
+          break;
+      }
+      console.log(input.selectionStart)
+      console.log(event.target)
+    }
+  }
+
+  // function insertAtCaret @Collin Anderson
+  function insertAtCaret(element, text) {
+    if (document.selection) {
+      element.focus();
+      let sel = document.selection.createRange();
+      sel.text = text;
+      element.focus();
+    } else if (element.selectionStart || element.selectionStart === 0) {
+      let startPos = element.selectionStart;
+      let endPos = element.selectionEnd;
+      element.value = element.value.substring(0, startPos) + text + element.value.substring(endPos, element.value.length);
+      element.focus();
+      element.selectionStart = startPos + text.length;
+      element.selectionEnd = startPos + text.length;
+    } else {
+      element.value += text;
+      element.focus();
+    }
+  }
+
+  function backspace(element) {
+    if (document.selection) {
+      element.focus();
+      let sel = document.selection.createRange();
+      --sel.text;
+      element.focus();
+    } else if (element.selectionStart || element.selectionStart === 0) {
+      let startPos = element.selectionStart;
+      let endPos = element.selectionEnd;
+      element.value = element.value.substring(0, startPos-1) + element.value.substring(endPos, element.value.length);
+      element.focus();
+      element.selectionStart = startPos;
+      element.selectionEnd = --endPos;
+    } else {
+      --element.value;
+      element.focus();
+    }
+  }
+
+  function capslockEvent(status) {
+    let keys = document.querySelectorAll('.App-keyboard-content .key')
+    for(let key of keys) {
+      switch(key.textContent) {
+        case 'backspace':
+        case 'whitespace':
+        case 'capslock':
+        case 'Ctrl+Alt':
+        case 'shift 1':
+        case 'shift 2':
+          break;
+        default:
+          status ?
+            key.textContent = key.textContent.toUpperCase()
+            :
+            key.textContent = key.textContent.toLowerCase()
+          break;
+      }
+    }
+  }
+
+  function ctrlAltEvent(status) {
+    let keys = document.querySelectorAll('.App-keyboard-content .key')
+    for(let key of keys) {
+      switch(key.textContent) {
+        case 'backspace':
+        case 'whitespace':
+        case 'capslock':
+        case 'Ctrl+Alt':
+        case 'shift 1':
+        case 'shift 2':
+          break;
+        case '1':
+        case '¹':
+          status ? key.textContent = '¹' : key.textContent = '1'
+          break;
+        case '2':
+        case '²':
+          status ? key.textContent = '²' : key.textContent = '2'
+          break;
+        case '3':
+        case '³':
+          status ? key.textContent = '³' : key.textContent = '3'
+          break;
+        case '4':
+        case '£':
+          status ? key.textContent = '£' : key.textContent = '4'
+          break;
+        case '5':
+        case '¢':
+          status ? key.textContent = '¢' : key.textContent = '5'
+          break;
+        case '6':
+        case '¬':
+          status ? key.textContent = '¬' : key.textContent = '6'
+          break;
+        case '=':
+        case '§':
+          status ? key.textContent = '§' : key.textContent = '='
+          break;
+        case '[':
+        case 'ª':
+          status ? key.textContent = 'ª' : key.textContent = '['
+          break;
+        case ']':
+        case 'º':
+          status ? key.textContent = 'º' : key.textContent = ']'
+          break;
+        default:
+          status ?
+            key.classList.add('hidden')
+            :
+            key.classList.remove('hidden')
+          break;
+      }
+    }
+  }
+
+  function shiftEvent(status) {
+    let keys = document.querySelectorAll('.App-keyboard-content .key')
+    for(let key of keys) {
+      switch(key.textContent) {
+        case 'backspace':
+        case 'whitespace':
+        case 'capslock':
+        case 'Ctrl+Alt':
+        case 'shift 1':
+        case 'shift 2':
+          break;
+        case '\'':
+        case '\'\'':
+          status ? key.textContent = '\'\'' : key.textContent = '\''
+          break;
+        case '1':
+        case '!':
+          status ? key.textContent = '!' : key.textContent = '1'
+          break;
+        case '2':
+        case '@':
+          status ? key.textContent = '@' : key.textContent = '2'
+          break;
+        case '3':
+        case '#':
+          status ? key.textContent = '#' : key.textContent = '3'
+          break;
+        case '4':
+        case '$':
+          status ? key.textContent = '$' : key.textContent = '4'
+          break;
+        case '5':
+        case '%':
+          status ? key.textContent = '%' : key.textContent = '5'
+          break;
+        case '6':
+        case '¨':
+          status ? key.textContent = '¨' : key.textContent = '6'
+          break;
+        case '7':
+        case '&':
+          status ? key.textContent = '&' : key.textContent = '7'
+          break;
+        case '8':
+        case '*':
+          status ? key.textContent = '*' : key.textContent = '8'
+          break;
+        case '9':
+        case '(':
+          status ? key.textContent = '(' : key.textContent = '9'
+          break;
+        case '0':
+        case ')':
+          status ? key.textContent = ')' : key.textContent = '0'
+          break;
+        case '-':
+        case '_':
+          status ? key.textContent = '_' : key.textContent = '-'
+          break;
+        case '=':
+        case '+':
+          status ? key.textContent = '+' : key.textContent = '='
+          break;
+        case '´':
+        case '`':
+          status ? key.textContent = '`' : key.textContent = '´'
+          break;
+        case '~':
+        case '^':
+          status ? key.textContent = '^' : key.textContent = '~'
+          break;
+        case '[':
+        case '{':
+          status ? key.textContent = '{' : key.textContent = '['
+          break;
+        case ']':
+        case '}':
+          status ? key.textContent = '}' : key.textContent = ']'
+          break;
+        case '\\':
+        case '|':
+          status ? key.textContent = '|' : key.textContent = '\\'
+          break;
+        case ',':
+        case '<':
+          status ? key.textContent = '<' : key.textContent = ','
+          break;
+        case '.':
+        case '>':
+          status ? key.textContent = '>' : key.textContent = '.'
+          break;
+        case ';':
+        case ':':
+          status ? key.textContent = ':' : key.textContent = ';'
+          break;
+        default:
+          status ?
+            key.textContent = key.textContent.toUpperCase()
+            :
+            key.textContent = key.textContent.toLowerCase()
+          break;
+      }
+    }
+  }
+
   return (
     <section className={ `App-keyboard${store.keyboard ? ' active' : '' }`} style={{ transform: `translate(${translateX}, ${translateY})` }} onMouseDown={onMouseDown}>
       <h1>português brasileiro</h1>
       <button className="exit" type="button" onClick={() => dispatch({type: 'TOGGLE_KEYBOARD'})}><i className="icon icon-exit">X</i></button>
-      <div className="App-keyboard-content">
+      <div className="App-keyboard-content" onClick={onKeyVirtual.bind(this)}>
         <div className="row">
           <button className="key" type="button">'</button>
           <button className="key" type="button">1</button>
@@ -71,7 +332,7 @@ function Keyboard({ store, dispatch }) {
           <button className="key" type="button">[</button>
         </div>
         <div className="row">
-          <button className="key" type="button" style={{width: '53.75px'}}><i className="icon icon-capslock">capslock</i></button>
+          <button className={ `key${capslock ? ' active' : '' }`} type="button" style={{width: '53.75px'}}><i className="icon icon-capslock">capslock</i></button>
           <button className="key" type="button">a</button>
           <button className="key" type="button">s</button>
           <button className="key" type="button">d</button>
@@ -86,7 +347,7 @@ function Keyboard({ store, dispatch }) {
           <button className="key" type="button">]</button>
         </div>
         <div className="row">
-          <button className="key" type="button" style={{width: '37.25px'}}><i className="icon icon-shitf">shitf 1</i></button>
+          <button className={ `key${shift ? ' active' : '' }`} type="button" style={{width: '37.25px'}}><i className="icon icon-shitf">shift 1</i></button>
           <button className="key" type="button">\</button>
           <button className="key" type="button">z</button>
           <button className="key" type="button">x</button>
@@ -98,12 +359,12 @@ function Keyboard({ store, dispatch }) {
           <button className="key" type="button">,</button>
           <button className="key" type="button">.</button>
           <button className="key" type="button">;</button>
-          <button className="key" type="button" style={{width: '86.75px'}}><i className="icon icon-shitf">shift 2</i></button>
+          <button className={ `key${shift ? ' active' : '' }`} type="button" style={{width: '86.75px'}}><i className="icon icon-shitf">shift 2</i></button>
         </div>
         <div className="row">
-          <button className="key" type="button" style={{width: '95px'}}>Ctrl+Alt</button>
+          <button className={ `key${ctrlalt ? ' active' : '' }`} type="button" style={{width: '95px'}}>Ctrl+Alt</button>
           <button className="key" type="button" style={{width: '293px', fontSize: '0px'}}>whitespace</button>
-          <button className="key" type="button" style={{width: '95px'}}>Ctrl+Alt</button>
+          <button className={ `key${ctrlalt ? ' active' : '' }`} type="button" style={{width: '95px'}}>Ctrl+Alt</button>
         </div>
       </div>
     </section>
