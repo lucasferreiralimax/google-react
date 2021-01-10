f45f15da26c6f2e1f759cb16991fce4f4e9d6d09
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-10 00:04:41 -0300

    link for repository github

--- src/components/Config/Config.js
+++ src/components/Config/Config.js
@@ -7,6 +7,8 @@ function Config() {
     <form className="App-config">
       <h1>Configurações</h1>
       <Darkmode />
+      <br/>
+      <a href="https://github.com/lucasferreiralimax/google-react" target="_blank">Estude o código fonte desse projeto no Github acesse aqui.</a>
     </form>
   );
 }
