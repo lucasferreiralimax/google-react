ccd3d7d207db6762eb5815d964588e488d2b5654
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-22 15:55:57 -0800

    page config

---
+++ src/components/Config/Config.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Config.css';
+import Darkmode from '../Darkmode/Darkmode';
+
+function Config() {
+  return (
+    <form data-testid="app-config" className="App-config">
+      <h1>Configurações</h1>
+      <Darkmode />
+      <br/>
+      <a href="https://github.com/lucasferreiralimax/google-react" target="_blank">Estude o código fonte desse projeto no Github acesse aqui.</a>
+    </form>
+  );
+}
+
+export default Config;
