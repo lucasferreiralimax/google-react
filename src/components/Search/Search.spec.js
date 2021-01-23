7043104809242ae99bb5d5348eacad5256e10bd6
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-22 17:31:25 -0800

    test: unit basic valid buttons Search

---
+++ src/components/Search/Search.spec.js
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import Search from './Search';
+import { Provider } from 'react-redux';
+import store from '../../store';
+
+describe("Should render <Search/>", () => {
+  it('renders valid buttons', () => {
+    render(
+    <Provider store={store}>
+      <Search />
+    </Provider>
+    );
+    const buttonKeyboard = screen.getByRole('button', { name: 'Icone Teclado Teclado virtual' })
+    const buttonVoice = screen.getByRole('button', { name: 'Buscar por voz' })
+    const buttonSearch = screen.getByRole('button', { name: 'Pesquisa Google' })
+    const buttonLucky = screen.getByRole('button', { name: 'Estou com sorte' })
+
+    expect(buttonKeyboard).toHaveTextContent('Teclado virtual');
+    expect(buttonVoice).toHaveTextContent('Buscar por voz');
+    expect(buttonSearch).toHaveTextContent('Pesquisa Google');
+    expect(buttonLucky).toHaveTextContent('Estou com sorte');
+  });
+});
