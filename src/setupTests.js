59edc01de7d238cdb9b4273cb2c261af89949761
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-22 16:41:44 -0800

    setupTest with getComputedStyle

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+const { getComputedStyle } = window;
+window.getComputedStyle = (elt) => getComputedStyle(elt);
