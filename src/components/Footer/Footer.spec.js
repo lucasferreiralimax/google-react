37396383fe332b68e367feaff771b27f547d6874
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-22 16:42:11 -0800

    test: unit with describe in Footer and Header

--- src/components/Footer/Footer.spec.js
+++ src/components/Footer/Footer.spec.js
@@ -4,13 +4,15 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from '../../store';
 
-it('renders app-darkmode', () => {
-  render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Footer />
-    </BrowserRouter>
-  </Provider>
-  );
-  expect(screen.getByTestId('app-darkmode')).toBeInTheDocument();
+describe("Should render <Footer/>", () => {
+  it('renders app-darkmode', () => {
+    render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Footer />
+      </BrowserRouter>
+    </Provider>
+    );
+    expect(screen.getByTestId('app-darkmode')).toBeInTheDocument();
+  });
 });
--- src/components/Header/Header.spec.js
+++ src/components/Header/Header.spec.js
@@ -1,13 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
-it('renders valid link and text', () => {
-  render(<Header />);
-  const textGodIsBig = screen.getByText(/Deus é Maior/i);
-  const altLogo = screen.getByAltText(/logo Google/i);
-  const linkReact = screen.getByText(/react/i);
 
-  expect(textGodIsBig).toHaveTextContent('Deus é Maior');
-  expect(altLogo).toBeDefined();
-  expect(linkReact).toBeInTheDocument();
+describe("Should render <Header/>", () => {
+  it('renders valid link and text', () => {
+    render(<Header />);
+    const textGodIsBig = screen.getByText(/Deus é Maior/i);
+    const altLogo = screen.getByAltText(/logo Google/i);
+    const linkReact = screen.getByText(/react/i);
+
+    expect(textGodIsBig).toHaveTextContent('Deus é Maior');
+    expect(altLogo).toBeDefined();
+    expect(linkReact).toBeInTheDocument();
+  });
 });
