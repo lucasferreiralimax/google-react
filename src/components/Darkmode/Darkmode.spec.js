e977f674221e6240862db7d7c5e57d994363c534
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-01-25 19:08:06 -0800

    test: unit basic render Darkmode

---
+++ src/components/Darkmode/Darkmode.spec.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import Darkmode from './Darkmode';
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from '../../store';
+
+describe("Should render <Darkmode/>", () => {
+  it('renders app-darkmode', () => {
+    render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <Darkmode />
+      </BrowserRouter>
+    </Provider>
+    );
+    expect(screen.getByTestId('app-darkmode')).toBeInTheDocument();
+  });
+});
