8b17ed3d28e27c81fdb9f8399c56ed4d19e80458
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-02-18 16:41:40 -0800

    test: unit testing capslock, shift 1 and 2, ctrl alt 1 and 2

--- src/components/Keyboard/Keyboard.spec.js
+++ src/components/Keyboard/Keyboard.spec.js
@@ -4,6 +4,31 @@ import Keyboard from './Keyboard';
 import { Provider } from 'react-redux';
 import store from '../../store';
 
+let keys_mock = ['\'', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '´', '[', 'capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ç', '~', ']', 'shift 1', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', ';', 'shift 2', 'Ctrl+Alt', 'whitespace', 'Ctrl+Alt']
+let keys_mock_shift = ['"', '!', '@', '#', '$', '%', '¨', '&', '*', '(', ')', '_', '+', 'backspace', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '`', '{', 'capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ç', '^', '}', 'shift 1', '\|', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', ':', 'shift 2', 'Ctrl+Alt', 'whitespace', 'Ctrl+Alt']
+let keys_mock_ctrl_alt = ['\'', '¹', '²', '³', '£', '¢', '¬', '7', '8', '9', '0', '-', '§', 'backspace', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '´', 'ª', 'capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ç', '~', 'º', 'shift 1', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', ';', 'shift 2', 'Ctrl+Alt', 'whitespace', 'Ctrl+Alt']
+
+function keyMockCheck(keys, mock, config) {
+  for(let [index, key] of keys.entries()) {
+    switch(key.textContent) {
+      case 'backspace':
+      case 'whitespace':
+      case 'capslock':
+      case 'Ctrl+Alt':
+      case 'shift 1':
+      case 'shift 2':
+        break;
+      default:
+        if(config.upper) {
+          expect(key.textContent).toBe(mock[index].toUpperCase())
+        } else {
+          expect(key.textContent).toBe(mock[index])
+        }
+        break;
+    }
+  }
+}
+
 describe("Should render <Keyboard/>", () => {
   beforeEach(() => {
     render(
@@ -13,15 +38,46 @@ describe("Should render <Keyboard/>", () => {
     );
   });
   it('renders app-keyboard', () => {
-
     expect(screen.getByTestId('app-keyboard')).toBeInTheDocument();
   });
   it('renders keys default', () => {
     let keys = document.querySelectorAll('.App-keyboard-content .key')
-    let keys_mock = ['\'', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '´', '[', 'capslock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ç', '~', ']', 'shift 1', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', ';', 'shift 2', 'Ctrl+Alt', 'whitespace', 'Ctrl+Alt']
 
     for(let [index, key] of keys.entries()) {
       expect(key.textContent).toBe(keys_mock[index])
     }
+  });
+  it('keyboard action capslock', () => {
+    fireEvent.click(screen.getByTestId('capslock'))
+    let keys = document.querySelectorAll('.App-keyboard-content .key')
+    keyMockCheck(keys, keys_mock, { upper: true} )
+  });
+  it('keyboard action shift', () => {
+    // active shift_1
+    fireEvent.click(screen.getByTestId('shift_1'))
+    let keys = document.querySelectorAll('.App-keyboard-content .key')
+    keyMockCheck(keys, keys_mock_shift, { upper: true} )
+
+    // disabled shift_1
+    fireEvent.click(screen.getByTestId('shift_1'))
+
+    // active shift_2
+    fireEvent.click(screen.getByTestId('shift_2'))
+    keys = document.querySelectorAll('.App-keyboard-content .key')
+    keyMockCheck(keys, keys_mock_shift, { upper: true} )
+  });
+  it('keyboard action ctrl-alt', () => {
+    // active ctrl_alt_1
+    fireEvent.click(screen.getByTestId('ctrl_alt_1'))
+    let keys = document.querySelectorAll('.App-keyboard-content .key')
+    keyMockCheck(keys, keys_mock_ctrl_alt, { upper: false} )
+
+    // disabled ctrl_alt_1
+    fireEvent.click(screen.getByTestId('ctrl_alt_1'))
+
+    // active ctrl_alt_2
+    fireEvent.click(screen.getByTestId('ctrl_alt_2'))
+    keys = document.querySelectorAll('.App-keyboard-content .key')
+    keyMockCheck(keys, keys_mock_ctrl_alt, { upper: false} )
   });
 });
