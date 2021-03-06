48fc692cd67c4ee70d2da55fe8a72c2faa89622a
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-05 22:27:35 -0800

    adjustment with animation button voice

--- src/components/Voice/Voice.js
+++ src/components/Voice/Voice.js
@@ -1,9 +1,10 @@
-import React, { useRef, useEffect }from 'react';
+import React, { useRef, useEffect, useState }from 'react';
 import { connect } from 'react-redux';
 import './Voice.scss';
 
 function Voice({ store, dispatch }) {
-  const buttoVoiceRef = useRef(null)
+  const buttoVoiceRef = useRef(null);
+  const [animation, setAnimation] = useState(false)
   let final_transcript = '';
   let recognizing = false;
   let ignore_onend;
@@ -14,7 +15,9 @@ function Voice({ store, dispatch }) {
      * Voice search setup
      */
 
-    voiceSetup();
+    if(store.voice) {
+      voiceSetup();
+    }
 
     /**
      * Clicked on outside of element
@@ -50,10 +53,12 @@ function Voice({ store, dispatch }) {
       recognition.onstart = function() {
         recognizing = true;
         document.querySelector('.App-voice p').textContent = 'Fale agora';
+        setAnimation(true);
         console.log('onstart voice');
       };
 
       recognition.onerror = function(event) {
+        setAnimation(false);
         if (event.error === 'no-speech') {
           console.log('onerror voice no-speech');
           ignore_onend = true;
@@ -76,6 +81,7 @@ function Voice({ store, dispatch }) {
         if (!final_transcript) {
           return;
         }
+        setAnimation(false);
         document.querySelector('.App-voice p').textContent = 'Fale agora';
       };
 
@@ -92,9 +98,10 @@ function Voice({ store, dispatch }) {
           document.querySelector('body').removeAttribute('style');
           dispatch({type: 'SEARCH_VALUE', payload: final_transcript})
           dispatch({type: 'SET_VOICE', payload: false})
+          recognition.stop();
         }
         if (interim_transcript) {
-          document.querySelector('.App-voice.active p').textContent = interim_transcript;
+          document.querySelector('.App-voice p').textContent = interim_transcript;
         }
       };
     }
@@ -111,11 +118,11 @@ function Voice({ store, dispatch }) {
   }
 
   return (
-    <section data-testid="app-voice" className={ `App-voice${store.voice ? ' active' : '' }`}>
+    <section data-testid="app-voice" className={`App-voice${store.voice ? ' active' : '' }`}>
       <button className="exit" type="button"><i className="icon icon-exit">X</i></button>
       <p>Fale agora</p>
-      <button type="button" className="btn-voice" ref={buttoVoiceRef} onClick={startButton}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#ea4335">
+      <button type="button" className={`btn-voice${animation ? ' active' : '' }`} ref={buttoVoiceRef} onClick={startButton}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
           <path d="M17 11.998c0 2.76-2.23 5-4.99 5l-.002.002a4.994 4.994 0 01-4.979-5h-2c0 3.52 2.59 6.433 5.98 6.92v3.078h.01V22h2v-3.08h-.01A6.982 6.982 0 0019 11.998z"/>
           <path fill="none" d="M0 0h24v24H0z"/>
           <path d="M12 15c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v7c0 1.66 1.34 3 3 3z"/>
