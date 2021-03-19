159e7079fa3f102f71c2ab16fcd379336c5c7b84
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-03-19 00:04:49 -0700

    adjustment images with small and medium size

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ function Header() {
   return (
     <header data-testid="app-header" className="App-header">
       <h1>Deus é Maior</h1>
-      <img src={logo} className="App-logo" alt="logo Google" width="272" />
+      <img src={logo} className="App-logo" alt="logo Google" width="272" height="92" />
       <h2><a href="https://pt-br.reactjs.org" target="_blank" rel="noreferrer noopener">React</a></h2>
     </header>
   );
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './Profile.scss';
-import foto from '../../assets/sinatra.jpg';
+import foto_40 from '../../assets/sinatra-40x40.jpg';
+import foto_150 from '../../assets/sinatra-150x150.jpg';
 
 function Profile() {
   const [show, setShow] = useState(false)
@@ -28,11 +29,11 @@ function Profile() {
     <section data-testid="app-profile" className="App-profile" ref={wrapperRef}>
       <h1 onClick={profile}>
         2L
-        <img src={foto} className="App-profile-foto" alt="Foto Frank Sinatra" width="40" />
+        <img src={foto_40} className="App-profile-foto" alt="Foto Frank Sinatra" width="40" height="40" />
       </h1>
       <div className={ `App-profile__content${show ? ' show' : '' }`}>
         <a href="https://github.com/lucasferreiralimax" target="_blank" rel="noreferrer noopener">
-          <img src={foto} className="App-profile-foto big" alt="Foto Frank Sinatra" />
+          <img src={foto_150} className="App-profile-foto big" alt="Foto Frank Sinatra" width="150" height="150" />
         </a>
         <h1>Lucas Lima</h1>
         <h2>lucasferreiralimax@gmail.com</h2>
