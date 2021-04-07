f476f1b4516b319f143112a46bd844ddf99defdc
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-04-06 23:32:53 -0700

    removing break line element and addition style

--- src/views/Config/Config.js
+++ src/views/Config/Config.js
@@ -12,8 +12,13 @@ function Config() {
       <form data-testid="app-config" className="App-config">
         <h1>Configurações</h1>
         <Darkmode />
-        <br/>
-        <a href="https://github.com/lucasferreiralimax/google-react" target="_blank" rel="noreferrer noopener">Estude o código fonte desse projeto no Github acesse aqui.</a>
+        <a
+          href="https://github.com/lucasferreiralimax/google-react"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Estude o código fonte desse projeto no Github acesse aqui.
+        </a>
       </form>
     </React.Fragment>
   );
