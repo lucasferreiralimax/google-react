68c673ba49f549f7da8b2a7086c85d2665a0246d
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-04-21 22:09:57 -0700

    select with change language with i18n

--- src/views/Config/Config.js
+++ src/views/Config/Config.js
@@ -1,27 +1,44 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Darkmode from '../../components/Darkmode/Darkmode';
 import Profile from '../../components/Profile/Profile';
+import { useTranslation } from 'react-i18next';
 
 import './Config.scss';
 
-function Config() {
+function Config({ store, dispatch }) {
+  const { t } = useTranslation();
+
   return (
     <React.Fragment>
       <Profile />
       <form data-testid="app-config" className="App-config">
-        <h1>Configurações</h1>
+        <h1>{t('text.config')}</h1>
         <Darkmode />
+        <select
+          className="App-language"
+          name="language"
+          value={store.lang}
+          onChange={e => dispatch({type: 'SET_LANG', payload: e.target.value})}
+        >
+          <option value="pt">{t('languages.pt')}</option>
+          <option value="en">{t('languages.en')}</option>
+        </select>
         <a
           href="https://github.com/lucasferreiralimax/google-react"
           target="_blank"
           rel="noreferrer noopener"
         >
-          Estude o código fonte desse projeto no Github acesse aqui.
+          {t('text.github')}
         </a>
       </form>
     </React.Fragment>
   );
 }
 
-export default Config;
+function mapStateToProps(state) {
+  return { store: state }
+}
+
+export default connect(mapStateToProps)(Config);
