dc54ae16fceb16eb771217fdd4b272e46e6a9a1e
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-04-21 22:09:32 -0700

    import i18n and app

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
 
 import Loading from './components/Loading/Loading';
 import Menu from './components/Menu/Menu';
@@ -18,31 +20,34 @@ const Login = lazy(() => import('./views/Login/Login'));
 const Config = lazy(() => import('./views/Config/Config'));
 const Notfound = lazy(() => import('./views/Notfound/Notfound'));
 
+
 function App() {
   return (
     <Provider store={store}>
-      <Suspense fallback={<Loading/>}>
-        <Router>
-          <div className="App">
-            <Menu />
-            <Switch>
-              <Route path="/login">
-                <Login />
-              </Route>
-              <Route path="/config">
-                <Config />
-              </Route>
-              <Route path="/" exact>
-                <Home />
-              </Route>
-              <Route path="*" exact>
-                <Notfound />
-              </Route>
-            </Switch>
-            <Footer />
-          </div>
-        </Router>
-      </Suspense>
+      <I18nextProvider i18n={i18n}>
+        <Suspense fallback={<Loading/>}>
+          <Router>
+            <div className="App">
+              <Menu />
+              <Switch>
+                <Route path="/login">
+                  <Login />
+                </Route>
+                <Route path="/config">
+                  <Config />
+                </Route>
+                <Route path="/" exact>
+                  <Home />
+                </Route>
+                <Route path="*" exact>
+                  <Notfound />
+                </Route>
+              </Switch>
+              <Footer />
+            </div>
+          </Router>
+        </Suspense>
+      </I18nextProvider>
     </Provider>
   );
 }
