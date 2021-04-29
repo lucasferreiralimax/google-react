337e1009046ad3cfcc44e4ce3fc24ce5fe24cb5f
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-04-29 01:45:44 -0700

    translate loading component

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import './Loading.scss';
 
 function Loading() {
+  const { t } = useTranslation();
+
   return (
     <div data-testid="app-loading" className="App-loading">
       <div data-testid="app-loading-loader" className="loader">
@@ -9,7 +12,7 @@ function Loading() {
           <circle className="path" cx="50" cy="50" r="20" fill="none" strokeWidth="2" strokeMiterlimit="10"/>
         </svg>
       </div>
-      <span>Loading...</span>
+      <span>{t('text.loading')}...</span>
     </div>
   );
 }
