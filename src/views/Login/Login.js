8afaae09a2def8906fc3dc16efa6e33470a900f8
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-05-01 02:27:31 -0700

    translate page login

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,8 +1,10 @@
 import React, { useState, useRef } from 'react';
+import { useTranslation } from 'react-i18next';
 import './Login.scss';
 import logo from '../../assets/logo.svg';
 
 function Login() {
+  const { t } = useTranslation();
   const [input, setInput] = useState('lucasferreiralimax@gmail.com')
   const [error, setError] = useState(false)
   const textInput = useRef(null);
@@ -29,8 +31,8 @@ function Login() {
         src={logo}
         width="75"
       />
-      <h1>Fazer login</h1>
-      <p>Use sua Conta do Google</p>
+      <h1>{t('login.title')}</h1>
+      <p>{t('login.description')}</p>
       <div className={ `App-login-control${input ? ' placeholder' : '' }${error ? ' error' : '' }`}>
         <input
           id="user"
@@ -43,14 +45,14 @@ function Login() {
           htmlFor="user"
           className="App-login-label"
         >
-          Email ou telefone
+          {t('login.form_email.label')}
         </label>
         { error && (
           <div className="App-login-error">
             <svg aria-hidden="true" fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path>
             </svg>
-            Digite um e-mail ou número de telefone
+            {t('login.form_email.error')}
           </div>
         )}
       </div>
@@ -58,11 +60,11 @@ function Login() {
         href="https://github.com/lucasferreiralimax"
         target="_blank"
         rel="noreferrer noopener">
-          Esqueceu seu e-mail?
+          {t('login.remember')}
       </a>
-      <p>Não está no seu computador? Use o modo visitante para fazer login com privacidade. <a href="https://github.com/lucasferreiralimax" target="_blank" rel="noreferrer noreferrer noopener">Saiba mais</a></p>
-      <button type="button" className="App-login-new btn">Criar conta</button>
-      <button type="button" className="App-login-next btn primary" onClick={submitNext}>Próxima</button>
+      <p>{t('login.text')} <a href="https://github.com/lucasferreiralimax" target="_blank" rel="noreferrer noreferrer noopener">{t('login.text_more')}</a></p>
+      <button type="button" className="App-login-new btn">{t('login.new')}</button>
+      <button type="button" className="App-login-next btn primary" onClick={submitNext}>{t('login.next')}</button>
     </form>
   );
 }
