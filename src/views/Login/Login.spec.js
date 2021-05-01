2fb23e8fc9474c3df2f5fb306aae5fe1b2074dc8
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-05-01 02:27:42 -0700

    test(unit): adjustment test page login

--- src/views/Login/Login.spec.js
+++ src/views/Login/Login.spec.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Login from './Login';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../../i18n';
 
 describe("Should render <Login/>", () => {
   it('renders valid link and text', () => {
-    render(<Login />);
-    const titleLogin = screen.getByText(/Fazer login/);
-    const titleParagraph = screen.getByText(/Use sua Conta do Google/i);
-    const labelEmail = screen.getByLabelText(/Email ou telefone/i);
-    const linkRemember = screen.getByText(/Esqueceu seu e-mail\?/i);
-    const buttonNewAccount = screen.getByRole('button', { name: 'Criar conta' })
-    const buttonNext = screen.getByRole('button', { name: 'Próxima' })
+    render(
+      <I18nextProvider i18n={i18n}>
+        <Login />
+      </I18nextProvider>
+    );
+    const titleLogin = screen.getByText(/login.title/);
+    const titleParagraph = screen.getByText(/login.description/i);
+    const labelEmail = screen.getByLabelText(/login.form_email.label/i);
+    const linkRemember = screen.getByText(/login.remember/i);
+    const buttonNewAccount = screen.getByRole('button', { name: 'login.new' })
+    const buttonNext = screen.getByRole('button', { name: 'login.next' })
 
-    expect(titleLogin).toHaveTextContent('Fazer login');
-    expect(titleParagraph).toHaveTextContent('Use sua Conta do Google');
+    expect(titleLogin).toHaveTextContent('login.title');
+    expect(titleParagraph).toHaveTextContent('login.description');
     expect(labelEmail).toBeInTheDocument();
-    expect(buttonNewAccount).toHaveTextContent('Criar conta');
-    expect(buttonNext).toHaveTextContent('Próxima');
-    expect(linkRemember).toHaveTextContent('Esqueceu seu e-mail?');
+    expect(buttonNewAccount).toHaveTextContent('login.new');
+    expect(buttonNext).toHaveTextContent('login.next');
+    expect(linkRemember).toHaveTextContent('login.remember');
   });
 });
