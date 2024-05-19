260e44ae20e62101f8d36155cf7a0581f9c26a15
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-05-19 19:26:33 -0300

    test(unit): adjustment import act

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { act } from "react-dom/test-utils";
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux';
-import { I18nextProvider } from 'react-i18next';
-import store from './store';
-import i18n from './i18n';
+import React, { act } from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { I18nextProvider } from "react-i18next";
+import store from "./store";
+import i18n from "./i18n";
 
 function AppWapper() {
   return (
@@ -20,44 +19,44 @@ function AppWapper() {
   );
 }
 
-const renderWithRouter = (ui, { route = '/' } = {}) => {
-  window.history.pushState({}, 'Test page', route)
-  return render(ui)
-}
+const renderWithRouter = (ui, { route = "/" } = {}) => {
+  window.history.pushState({}, "Test page", route);
+  return render(ui);
+};
 
 beforeEach(async () => {
   await act(async () => {
-    renderWithRouter(<AppWapper />, { route: '/' });
+    renderWithRouter(<AppWapper />, { route: "/" });
   });
 });
 
 describe("Should render <App/>", () => {
-  it('renders app-nav', () => {
-    expect(screen.getByTestId('app-nav')).toBeInTheDocument();
+  it("renders app-nav", () => {
+    expect(screen.getByTestId("app-nav")).toBeInTheDocument();
   });
 
-  it('renders app-header', () => {
-    expect(screen.getByTestId('app-header')).toBeInTheDocument();
+  it("renders app-header", () => {
+    expect(screen.getByTestId("app-header")).toBeInTheDocument();
   });
 
-  it('renders app-profile', () => {
-    expect(screen.getByTestId('app-profile')).toBeInTheDocument();
+  it("renders app-profile", () => {
+    expect(screen.getByTestId("app-profile")).toBeInTheDocument();
   });
 
-  it('renders app-keyboard', () => {
-    expect(screen.getByTestId('app-keyboard')).toBeInTheDocument();
+  it("renders app-keyboard", () => {
+    expect(screen.getByTestId("app-keyboard")).toBeInTheDocument();
   });
 
-  it('renders app-voice', () => {
-    expect(screen.getByTestId('app-voice')).toBeInTheDocument();
+  it("renders app-voice", () => {
+    expect(screen.getByTestId("app-voice")).toBeInTheDocument();
   });
 
-  it('renders app-search', () => {
-    expect(screen.getByTestId('app-search')).toBeInTheDocument();
+  it("renders app-search", () => {
+    expect(screen.getByTestId("app-search")).toBeInTheDocument();
   });
 
-  it('renders app-footer', () => {
-    expect(screen.getByTestId('app-footer')).toBeInTheDocument();
+  it("renders app-footer", () => {
+    expect(screen.getByTestId("app-footer")).toBeInTheDocument();
   });
 });
 
